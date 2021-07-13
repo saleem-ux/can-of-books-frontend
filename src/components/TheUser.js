6290605fd10ce3a8991e6ab1760ca120f0f86fd9
MAMOUN-kamal-alshisani <mamoun.alshishani@yahoo.com>
2021-07-13 21:15:06 +0300

    update

--- src/components/TheUser.js
+++
@@ -1,6 +0,0 @@
-
-
-import React, { Component } from 'react';
-import { Card, Button } from 'react-bootstrap/'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { withAuth0 } from '@auth0/auth0-react';
